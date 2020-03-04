d042927d3511e134985e658ba2912a1bfb8466de
xxzhang <xiaoxi.zhang@baoying.com>
2020-03-04 21:10:00 +0800

    add new component of moon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
 import { SunComponent } from './sun/sun.component';
+import { MoonComponent } from './moon/moon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SunComponent
+    SunComponent,
+    MoonComponent
   ],
   imports: [
     BrowserModule
