6d0cca8e35ea74ff496bec63b1d634ff6fbaefa2
xxzhang <xiaoxi.zhang@baoying.com>
2020-03-06 22:57:19 +0800

    add basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
 import { SunComponent } from './sun/sun.component';
 import { MoonComponent } from './moon/moon.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SunComponent,
-    MoonComponent
+    MoonComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
